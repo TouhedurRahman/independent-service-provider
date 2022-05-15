c9a899ffdeeda329d9dfc7191a64fcab9ea26c77
TouhedurRahman <touhedur.cse@gmail.com>
2022-05-16 00:30:16 +0600

    sign up and sign in with email and password authentication added

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,43 +1,107 @@
-import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useRef, useState } from 'react';
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import auth from '../../firebase.init';
+import Loading from '../Loading/Loading';
 import SocialLogin from '../SocialLogin/SocialLogin';
 import './Login.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
+    const [passwordShown, setPasswordShown] = useState(false);
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    const [
+        sendPasswordResetEmail,
+        sending
+    ] = useSendPasswordResetEmail(auth);
+
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
 
     const navigate = useNavigate();
+    const location = useLocation();
+    const from = location.state?.from?.pathname || "/";
+
+    let errorElement;
+
+    if (error) {
+        errorElement = <p className='text-denger'>Error: {error?.message}</p>
+    }
+
+    if (user) {
+        navigate(from, { replace: true });
+    }
+
+    if (loading || sending) {
+        return <Loading></Loading>
+    }
+
+    const togglePassword = () => {
+        setPasswordShown(!passwordShown);
+    };
+
+    const handleOnSubmit = event => {
+        event.preventDefault();
+
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+
+        console.log(email, password);
+
+        signInWithEmailAndPassword(email, password);
+    }
 
     const navigateRegister = event => {
         navigate('/register');
     }
 
+    const resetPassword = async () => {
+        const email = emailRef.current.value;
+        if (email) {
+            await sendPasswordResetEmail(email);
+            toast('Email sent!');
+        }
+        else {
+            toast("Plaease enter your email address!");
+        }
+    }
+
     return (
         <div>
             <div className='w-50 mx-auto border border-success p-5 mt-5 mb-5 rounded-3'>
-                <form>
+                <form onSubmit={handleOnSubmit}>
                     <h1 className='text-center text-primary'>Login</h1>
                     <div class="mb-3">
                         <label for="exampleInputEmail" class="form-label">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail" />
+                        <input type="email" ref={emailRef} class="form-control" id="exampleInputEmail" required />
                     </div>
                     <div class="mb-3">
                         <label for="exampleInputPassword" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword" />
+                        <input type={passwordShown ? "text" : "password"} ref={passwordRef} class="form-control" id="exampleInputPassword" required />
                     </div>
                     <div class="mb-3 form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+                        <input type="checkbox" class="form-check-input" onClick={togglePassword} id="exampleCheck1" />
                         <label class="form-check-label" for="exampleCheck1">Show Password</label>
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
-
+                {errorElement}
                 <p>
-                    Forget password? <button className='btn btn-link text-primary text-decoration-none pe-auto'>Reset Password</button>
+                    Forget password?<button className='btn btn-link text-primary text-decoration-none pe-auto' onClick={resetPassword}>Reset Password</button>
                 </p>
                 <p>
                     New to Food Mart? <Link to="/register" className='text-primary text-decoration-none pe-auto' onClick={navigateRegister}>Please Register</Link>
                 </p>
                 <SocialLogin></SocialLogin>
+                <ToastContainer />
             </div>
         </div>
     );
--- src/Components/Registration/Registration.js
+++ src/Components/Registration/Registration.js
@@ -1,39 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
 import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
+import Loading from '../Loading/Loading';
 import SocialLogin from '../SocialLogin/SocialLogin';
+import { ToastContainer, toast } from 'react-toastify';
 import './Registration.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Registration = () => {
+    const [passwordShown, setPasswordShown] = useState(false);
+
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+    ] = useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
+
+    const [updateProfile, updating] = useUpdateProfile(auth);
+
     const navigate = useNavigate();
 
     const navigateLogin = () => {
         navigate('/login');
     }
 
+    if (user) {
+        console.log('user', user);
+    }
+
+    if (loading || updating) {
+        return <Loading></Loading>
+    }
+
+    const togglePassword = () => {
+        setPasswordShown(!passwordShown);
+    };
+
+    const handleRegister = async (event) => {
+        event.preventDefault();
+
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        const confirmPassword = event.target.confirmPassword.value;
+
+        if (password === confirmPassword) {
+            await createUserWithEmailAndPassword(email, password);
+            await updateProfile({ displayName: name });
+            alert('Updated profile');
+            navigate('/home');
+        }
+        else {
+            toast("Password and confirm password doesn't  same!");
+        }
+    }
+
     return (
         <div>
             <div className='w-50 mx-auto border border-success p-5 mt-5 mb-5 rounded-3'>
-                <form>
+                <form onSubmit={handleRegister}>
                     <h1 className='text-center text-primary'>Please Register</h1>
                     <div class="mb-3">
                         <label for="exampleInputName" class="form-label">Name</label>
-                        <input type="email" class="form-control" id="exampleInputName" />
+                        <input type="text" name="name" class="form-control" id="exampleInputName" required />
                     </div>
                     <div class="mb-3">
                         <label for="exampleInputEmail" class="form-label">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail" />
+                        <input type="email" name="email" class="form-control" id="exampleInputEmail" required />
                     </div>
                     <div class="mb-3">
                         <label for="exampleInputPassword1" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" />
+                        <input type={passwordShown ? "text" : "password"} name="password" class="form-control" id="exampleInputPassword1" required />
                     </div>
                     <div class="mb-3">
                         <label for="exampleInputPassword2" class="form-label">Confirm Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword2" />
+                        <input type={passwordShown ? "text" : "password"} name="confirmPassword" class="form-control" id="exampleInputPassword2" required />
                     </div>
                     <div class="mb-3 form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                        <input type="checkbox" class="form-check-input" onClick={togglePassword} id="exampleCheck1" />
+                        <label class="form-check-label" for="exampleCheck1">Show Password</label>
                     </div>
                     <button type="submit" class="btn btn-primary">Register</button>
                 </form>
@@ -42,6 +88,7 @@ const Registration = () => {
                     Already have an account? <Link to="/login" className='text-danger text-decoration-none pe-auto' onClick={navigateLogin}>Please Login</Link>.
                 </p>
                 <SocialLogin></SocialLogin>
+                <ToastContainer />
             </div>
         </div>
     );
