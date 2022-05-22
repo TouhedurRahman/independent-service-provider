064c98d578bf94ac77a9409d5ea7655435f74e3b
TouhedurRahman <touhedur.cse@gmail.com>
2022-05-23 00:43:03 +0600

    Verification section added

--- src/Components/RequireAuth/RequireAuth.js
+++ src/Components/RequireAuth/RequireAuth.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import './RequireAuth.css';
+import { useAuthState, useSendEmailVerification } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import auth from '../../firebase.init';
 import Loading from '../Loading/Loading';
+import Footer from '../Footer/Footer';
 
 const RequireAuth = ({ children }) => {
-    const [user, loading] = useAuthState(auth);
+    const [
+        user,
+        loading
+    ] = useAuthState(auth);
+
+    const [sendEmailVerification] = useSendEmailVerification(auth);
+
     let location = useLocation();
 
     if (loading) {
@@ -13,11 +23,56 @@ const RequireAuth = ({ children }) => {
     }
 
     if (!user) {
-        // Redirect them to the /login page, but save the current location they were
-        // trying to go to when they were redirected. This allows us to send them
-        // along to that page after they login, which is a nicer user experience
-        // than dropping them off on the home page.
         return <Navigate to="/login" state={{ from: location }} replace />;
+    }
+
+    if (!user.emailVerified) {
+        return (
+            <div className='text-center'>
+                <div className='m-3 mx-auto'>
+                    <img style={
+                        {
+                            borderRadius: '50%',
+                            width: '300px',
+                            height: '150px'
+                        }
+                    }
+                        src="../images/email/email.jpg" alt="email-notification" />
+                </div>
+                <div>
+                    <h1 className='text-success'>Please, Verify your Account</h1>
+                    <div className="mx-auto m-3" style={{ maxWidth: '280px', }}>
+                        <p>
+                            Thanks for signup with us.
+                            <br />
+                            Your account is <span className='text-danger'>not verified</span> yet.
+                            <br />
+                            Please Cheak your email
+                            <br />
+                            <span className='text-danger'>
+                                ({user.email})
+                            </span>
+                            <br />
+                            and verify your account.
+                            If you don't get any verification email, click the button bellow to get verification email again.
+                        </p>
+                    </div>
+                    <button onClick={
+                        async () => {
+                            await sendEmailVerification();
+                            toast('Email sent!');
+                        }
+                    } class="resend-email-btn">Re-send Verification</button>
+                    <div className="mx-auto mt-3 mb-5" style={{ maxWidth: '280px', }}>
+                        <p>
+                            If this email wasn't intended for you feel free to delete it, Thak you.
+                        </p>
+                    </div>
+                    <ToastContainer />
+                    <Footer></Footer>
+                </div>
+            </div>
+        )
     }
 
     return children;
